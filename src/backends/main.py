60f0c77ea5b6fe0be9d7302fdb741dbcec65560a
Om Thorat <thoratom1104@gmail.com>
2024-08-25 19:18:23 +0530

    Home page

--- src/backends/main.py
+++ src/backends/main.py
@@ -1,11 +1,20 @@
 import flask
+import getpass
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def index():
     return "Hello, World!"
+
+
+@app.route("/user")
+def user():
+    username = getpass.getuser()
+    return username
 
 
 app.run(host="localhost", port=6969, debug=True)
