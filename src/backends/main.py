a559b62c05def591e7d87b15daafe8f44c0092fe
Om Thorat <thoratom1104@gmail.com>
2024-08-26 03:07:50 +0530

    Restore windows

--- src/backends/main.py
+++ src/backends/main.py
@@ -4,6 +4,7 @@ import getpass
 from flask_cors import CORS
 from PIL import ImageGrab
 import io
+from scripts import get_windows
 
 
 app = flask.Flask(__name__)
@@ -20,21 +21,26 @@ def user():
     username = getpass.getuser()
     return username
 
+
 def capture_screenshot():
     screenshot = ImageGrab.grab()
     return screenshot
 
+
 @app.route("/screenshot")
-def screenshot(): 
+def screenshot():
     screenshot = capture_screenshot()
     img_io = io.BytesIO()
-    screenshot.save(img_io, 'PNG')
+    screenshot.save(img_io, "PNG")
     img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
+    return send_file(img_io, mimetype="image/png")
 
 
-
-
+@app.route("/getwindows")
+def getwindows():
+    x = get_windows()
+    print(x)
+    return x
 
 
 app.run(host="localhost", port=6969, debug=True)
--- src/backends/scripts/getwindows.py
+++ src/backends/scripts/getwindows.py
@@ -8,10 +8,6 @@ def enum_windows_callback(hwnd, results):
         results.append(hwnd)
 
 
-windows = []
-win32gui.EnumWindows(enum_windows_callback, windows)
-
-
 def get_window_details(hwnd):
 
     title = win32gui.GetWindowText(hwnd)
@@ -27,21 +23,33 @@ def get_window_details(hwnd):
     width, height = rect[2] - rect[0], rect[3] - rect[1]
 
     return {
-        'title': title,
-        'exe_path': exe_path,
-        'x': x,
-        'y': y,
-        'width': width,
-        'height': height
+        "title": title,
+        "exe_path": exe_path,
+        "x": x,
+        "y": y,
+        "width": width,
+        "height": height,
     }
 
-blacklist_path = ["C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe", "C:\Windows\System32\ApplicationFrameHost.exe", "C:\Windows\ImmersiveControlPanel\SystemSettings.exe"]
+
+blacklist_path = [
+    "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe",
+    "C:\Windows\System32\ApplicationFrameHost.exe",
+    "C:\Windows\ImmersiveControlPanel\SystemSettings.exe",
+]
 blacklist_title = ["Program Manager"]
 
-for hwnd in windows:
-    details = get_window_details(hwnd)
-    if details['exe_path'] not in blacklist_path and details['title'] not in blacklist_title:
-        print(f"Title: {details['title']}")
-        print(f"Executable Path: {details['exe_path']}")
-        print(f"Position: ({details['x']}, {details['y']})")
-        print(f"Size: {details['width']}x{details['height']}\n")
+
+def get_window_list():
+    windows = []
+    win32gui.EnumWindows(enum_windows_callback, windows)
+    for hwnd in windows:
+        details = get_window_details(hwnd)
+        if (
+            details["exe_path"] not in blacklist_path
+            and details["title"] not in blacklist_title
+        ):
+            print(f"Title: {details['title']}")
+            print(f"Executable Path: {details['exe_path']}")
+            print(f"Position: ({details['x']}, {details['y']})")
+            print(f"Size: {details['width']}x{details['height']}\n")
