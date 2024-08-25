3565dbc23114ee9cf3da31de0f238ccbbc5951c1
Om Thorat <thoratom1104@gmail.com>
2024-08-25 17:31:44 +0530

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,28 +1,56 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
+"use client"
+
+import { AppShell, Burger, ColorSchemeScript, Group, MantineProvider, Skeleton } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { ActionIcon, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import { IconSun, IconMoon } from '@tabler/icons-react';
-import cx from 'clsx';
-import classes from './Demo.module.css';
-
-export const metadata = {
-  title: 'My Mantine app',
-  description: 'I have followed setup instructions carefully',
-};
+// export const metadata = {
+//   title: 'My Mantine app',
+//   description: 'I have followed setup instructions carefully',
+// };
 
 export default function RootLayout({
   children,
 }) {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure(false);
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(false);
+
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider>
+        <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
+          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+          {/* <MantineLogo size={30} /> */}
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+        </MantineProvider>
       </body>
     </html>
   );
